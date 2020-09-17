379b637226a5fec7ad43b2e5e93593a64e849efe
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-17 19:50:20 -0400

    add uds for radar check

---
+++ selfdrive/car/disable_radar_uds.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from tqdm import tqdm
+
+from panda import Panda
+from panda.python.uds import UdsClient, NegativeResponseError, SESSION_TYPE, CONTROL_TYPE, MESSAGE_TYPE
+
+ADDR=0x7d0
+
+if __name__ == "__main__":
+  panda = Panda()
+  panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
+  uds_client = UdsClient(panda, ADDR, 2, timeout=0.1, debug=True)
+  uds_client.diagnostic_session_control(SESSION_TYPE.EXTENDED_DIAGNOSTIC)
+  # messages that work
+  #data = uds_client.communication_control(CONTROL_TYPE.EXTENDED_DIAGNOSTIC, MESSAGE_TYPE.NORMAL_AND_NETWORK_MANAGEMENT)
+  #data = uds_client.communication_control(CONTROL_TYPE.DISABLE_RX_DISABLE_TX | 0x80, MESSAGE_TYPE.NORMAL_AND_NETWORK_MANAGEMENT)
+  #exit(0)
+
+  print("querying addresses ...")
+  l = list(range(0x7d0))
+  with tqdm(total=len(l)) as t:
+    for i in l:
+      ct = i >> 8
+      mt = i & 0xFF
+      t.set_description(f"{hex(ct)} - {hex(mt)}")
+      try:
+        data = uds_client.diagnostic_session_control(ct, mt)
+        print(f"\n{ct} - {mt}: success")
+      except NegativeResponseError as e:
+        if e.message != "COMMUNICATION_CONTROL - sub-function not supported" and e.message != "COMMUNICATION_CONTROL - request out of range":
+          print(f"\n{ct} - {mt}: {e.message}")
+      t.update(1)
