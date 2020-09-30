7ae8c9b4eefabda86b14e7d2d678ffd1f9df5d4b
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-30 18:02:55 -0400

    Update long_mpc.py

--- selfdrive/controls/lib/long_mpc.py
+++ selfdrive/controls/lib/long_mpc.py
@@ -124,7 +124,7 @@ class LongitudinalMpc():
 
     TR = clip(TR, 0.7, maxTR)
     if v_ego < 5. and self.v_lead > v_ego + 1.1:
-      TR = self.last_TR - .025
+      TR = 0.5
       TR = clip(TR, 0.25, maxTR)
     self.last_TR = TR
 
