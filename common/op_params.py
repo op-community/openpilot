5f24e73f7d16e66f6ee233ab5c6fe114780c525f
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-10-02 23:02:54 -0400

    update

--- common/op_params.py
+++ common/op_params.py
@@ -61,7 +61,10 @@ class opParams:
     """
 
     VT = ValueTypes()
-    self.fork_params = {'alca_min_speed': Param(25.0, VT.number, 'The minimum speed for an automatic lane change MPH/KPH'),
+    self.fork_params = {'Camera_Offset': Param(0.06, VT.number, 'This will reposition your vehicle if there has a lane hugging issue'
+                                                                'increase the offset(max 0.12) if the vehicle is hugging right lane'
+                                                                'decrease the offset(min 0.0) if the vehicle is hugging left lane'),
+                        'ALC_Min_Speed': Param(25.0, VT.number, 'The minimum speed for an automatic lane change MPH/KPH'),
                         'Radar_Disable_Activate': Param(False, bool, 'Sends UDS to Disable Stock Radar'),
                         'MDPS_Harness_Present': Param(True, bool, 'MDPS harness present'),
                         'SCC_Present': Param(True, bool, 'Car has stock SCC'),
